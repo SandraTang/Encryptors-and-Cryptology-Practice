3dda4fe0977c473ed1aa38c3f68a3bb887f6b570
Sandra Tang <24090@students.ausd.net>
2019-06-17 15:47:11 -0700

    Create alphanumeric-english.py

---
+++ alphanumeric-english.py
@@ -0,0 +1,24 @@
+#Letter Numbers Quiz Game (English)
+#By Sandra Tang
+
+from random import randint
+
+print "Letter Numbers Quiz Game (English)"
+
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+print "Alphanumeric Quiz"
+
+print "State the correlating number to the letter."
+
+while 2 == 2:
+  answer = randint(0, len(alphabet)-1)
+  print alphabet[answer]
+  guess = ""
+  
+  while not answer == guess:
+      guess = raw_input("Guess: ")
+      guess = int(guess)
+  
+  print "You got it! Here's another."
+  print
